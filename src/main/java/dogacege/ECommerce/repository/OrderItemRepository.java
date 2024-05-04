a9ca2b5e2a10d22ab7a7a63266600ca7e7113c9c
DogacUlgacc <ulgac1907@gmail.com>
2024-05-04 16:26:59 +0300

    OrderItem

--- src/main/java/dogacege/ECommerce/repository/OrderItemRepository.java
+++ src/main/java/dogacege/ECommerce/repository/OrderItemRepository.java
@@ -1,4 +1,9 @@
 package dogacege.ECommerce.repository;
 
-public interface OrderItemRepository {
+import dogacege.ECommerce.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
 }
--- src/main/java/dogacege/ECommerce/service/OrderItemService.java
+++ src/main/java/dogacege/ECommerce/service/OrderItemService.java
@@ -1,4 +1,41 @@
 package dogacege.ECommerce.service;
 
+
+import dogacege.ECommerce.entity.OrderItem;
+import dogacege.ECommerce.repository.OrderItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class OrderItemService {
+    private final OrderItemRepository orderItemRepository;
+
+    public OrderItemService(OrderItemRepository orderItemRepository) {
+        this.orderItemRepository = orderItemRepository;
+    }
+
+    public OrderItem getOrderItemById(Long id) {
+     return orderItemRepository.findById(id).orElse(null);
+    }
+
+    public List<OrderItem> getAllOrderItem() {
+        return orderItemRepository.findAll();
+    }
+
+
+    public OrderItem updateOrderItem(Long id, OrderItem updatedOrderItem) {
+        OrderItem existingOrderItem = orderItemRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("id" + id));
+
+        // Güncelleme işlemleri
+        existingOrderItem.setProduct(updatedOrderItem.getProduct());
+        existingOrderItem.setQuantity(updatedOrderItem.getQuantity());
+        existingOrderItem.setPrice(updatedOrderItem.getPrice());
+
+
+        return orderItemRepository.save(existingOrderItem);
+    }
+
 }
+
